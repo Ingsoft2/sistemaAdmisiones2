1af3988c33a26e38c85902a723248d80d265a723
Fernanda <Fernanda@Fernanda-PC>
2015-03-24 19:54:04 -0500

    gestin programa

---
+++ fooder.php
@@ -0,0 +1,7 @@
+    <footer>
+            <div id="footer">
+                <h3 align="center">Univerdidad Mariana</h3>
+                <h3 align="center">Web Solutions</h3>
+                <h3 align="center">2015</h3>
+            </div>
+    </footer>
--- index.php
+++ index.php
@@ -23,13 +23,9 @@ include("gestionarFacultad.php");
 ?>
             </div>
         </section>
+        <?php
+include("fooder.php"); 
+?>
         
-        <footer>
-            <div id="footer">
-                <h3 align="center">Univerdidad Mariana</h3>
-                <h3 align="center">Web Solutions</h3>
-                <h3 align="center">2015</h3>
-            </div>
-        </footer>
     </body>
 </html>
