7c77ddc64f01e2b63581f3b94b20a371d0d9f989
JUANCARLOS <JUANCARLOS@JUANK-PC>
2015-03-30 23:18:57 -0500

    nueva base de datos falta foranea programa estudiante

---
+++ admin/aspirante/agregarAspirante.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link type="text/css" rel="stylesheet" href="../../css/style.css">
+        <title></title>
+    </head>
+    <?php include ("Header.php"); ?>
+    <?php include ("../conexion.php"); ?>
+    <body>
+        <div id="section">
+        <br>
+        
+        <form method="POST" enctype="multipart/form-data" action="procesar_aspirante.php" >
+            <table border="1">
+                <thead>
+                    <tr align ="center">
+                        
+                        <th colspan="2">Datos aspirante</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Identificación:</td>
+                        <td><input type="text" name="txt_identificacion" value="" required=""/></td>
+                    </tr>
+                    <tr>
+                        <td>Nombres:</td>
+                        <td><input type="text" name="txt_nombres" value="" required=""/></td>
+                    </tr>
+                    <tr>
+                        <td>Apellidos:</td>
+                        <td><input type="text" name="txt_apellidos" value="" required=""/></td>
+                    </tr>
+                    <tr>
+                        <td>Fecha de nacimiento:</td>
+                        <td><input type="date" name="txt_nacimiento" value="" required=""/></td>
+                    </tr>
+                    <tr>
+                        <td>Lugar de nacimiento</td>
+                        <td><input type="text" name="txt_lugarnacimiento" value="" required=""/></td>
+                    </tr>
+                    <tr>
+                        <td>Género:</td>
+                        <td>Masculino<input type="radio" name="genero" value="Masculino"/>Femenino<input type="radio" name="genero" value="Femenino"></td>
+                    </tr>
+                    <tr>
+                        <td>Colegio:</td>
+                        <td> 
+                            <?php $result = mysql_query("SELECT * FROM colegio"); ?>
+                            <select name="txt_colegio"> <?php  while ($row = mysql_fetch_row($result)) 
+                                     {  ?>
+                                        
+                                        <option value="<?php echo $row[0]; ?>"><?php echo $row[1] ?></option>
+                                    <?php } ?>
+                                     {
+                        } 
+                        ?>
+                            </select></td>
+                    </tr>
+                    <tr>
+                        <td>Promedio:</td>
+                        <td><input type="text" name="txt_promedio" value="" required=""td>
+                    </tr>
+                    <tr>
+                        <td>Foto:</td>
+                        <td><input type="file" name="foto" id="foto" /></td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+            
+            <table>
+                <tr>
+                    <td>
+            <input type="submit" value="Enviar" class="boton"/>
+            <input type="hidden" value="Enviar" name="req_asp">
+            </td>
+            </tr>
+            </table>
+
+        </form>
+        </div>
+    </body>
+         <?php
+include("fooder.php"); 
+?>
+    
+</html>
