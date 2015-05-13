3e447bb198af15684cbbb790a0ea22d8b6084fe9
Miguel Benavides <migue.abf@gmail.com>
2015-05-13 17:47:03 -0500

    Validacion

--- admin/facultad/procesar_facultad.php
+++ admin/facultad/procesar_facultad.php
@@ -9,9 +9,17 @@ switch ($_REQUEST['req_fac'])
         
         $nombre = $_POST['txt_nombres'];
         $fechaCreacion = $_POST['txt_creacion'];
-                
+        
+        if(strlen($nombre)>3 && strlen($nombre)<45)
+               
+            
+             {
         $mensaje = Facultad::insertarFacultad($nombre, $fechaCreacion);
      header('Location:../gestiones/gestionarFacultad.php'); 
+      }
+            else {echo '<script> alert("el nombre de la facultad debe ser  mayor a 3 letras y menor a 45"); location.href="../facultad/agregarFacultad.php";</script>';  }
+         
+        
       exit();
       
       case "eliminar":
@@ -35,8 +43,13 @@ if(isset($_POST['txt_id']))
         
          $nombre = $_POST['txt_nombre'];
         $estado = $_POST['txt_fechaCreacion']; 
-        Colegio::editarColegio($id, $nombre, $fechaCreacion);
+        
+        if(strlen($nombre)>3 && strlen($nombre)<45)
+              {
+            Facultad::editarFacultad($id, $nombre, $fechaCreacion);
         header('Location:../gestiones/gestionarFacultad.php');
+         }
+            else {echo '<script> alert("el nombre de la facultad debe ser  mayor a 3 letras y menor a 45"); location.href="../facultad/agregarFacultad.php";</script>';  }
     }
     else
         echo 'id menor a 0';
